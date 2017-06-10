f9da1596ee0c6c1d89478b70dd02dd69f9ea7f64
Naz <nazmalik08@gmail.com>
2017-06-10 16:43:02 +0100

    Create spec and src files, include Dusty in lib

---
+++ spec/frequencyCounterSpec.js
@@ -0,0 +1,30 @@
+describe('initialize', function(){
+  it('has a placeholder', function(){
+    var note = new frequencyCounter();
+    // assert.isEq(note.text, note.PLACEHOLDER);
+  })
+})
+
+// describe ('gets block', function(){
+//   it('is a failing test', function(){
+//     var note = new Note();
+//     assert.isTrue(note.getText() === "baboon");
+//   });
+
+  // it('gets text', function(){
+  //   var note = new Note();
+  //   assert.isTrue(note.getText() === note.PLACEHOLDER);
+  // })
+// });
+
+// describe ('gets another block', function(){
+//   it('gets more text', function(){
+//    var note = new Note();
+//    assert.isTrue(note.getText() === note.PLACEHOLDER);
+//   })
+
+  // it('gets yet more text', function(){
+  //  var note = new Note();
+  //  assert.isTrue(note.getText() === note.PLACEHOLDER);
+  // })
+// });
