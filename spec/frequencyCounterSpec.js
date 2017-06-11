ca77134274da47c18590b7cd9daa30739c087e60
Naz <nazmalik08@gmail.com>
2017-06-11 14:05:01 +0100

    Add split string function

--- spec/frequencyCounterSpec.js
+++ spec/frequencyCounterSpec.js
@@ -1,21 +1,20 @@
-describe('FrequencyCounter', function() {
+ describe('FrequencyCounter', function() {
 
-  beforeEach(function() {
-    bookText = "This is a test string"
-    fc = new FrequencyCounter(bookText);
-  });
+   beforeEach(function() {
+     bookText = "This is a test string"
+     fc = new FrequencyCounter(bookText);
+   });
 
-  describe('initialize', function() {
-    it('has text', function() {
-      expect(fc.getText).toEqual("This is a test string");
-    })
-  });
+   describe('initialize', function() {
+     it('has text', function() {
+       expect(fc.text).toEqual("This is a test string");
+     })
+   });
 
-  describe('split words', function() {
-    it('splits words when called', function() {
-      fc.splitWords();
-      expect(fc.splitWords()).toEqual(["THE"])
-    })
-  })
+   describe('split words', function() {
+     it('splits words when called', function() {
+       expect(fc.splitWords()).toEqual(["This","is","a","test","string"])
+     })
+   })
+ })
 
-})
--- src/frequencyCounter.js
+++ src/frequencyCounter.js
@@ -1,20 +1,13 @@
 'use strict';
 
-var fs = require('fs');
-var bookText = fs.readFileSync("./text-files/railway-children.txt", "utf8");
+// var fs = require('fs');
+// var bookText = fs.readFileSync("./text-files/railway-children.txt", "utf8");
 
 function FrequencyCounter(text) {
   this.text = text
 }
 
-FrequencyCounter.prototype.getText = function() {
-  return this.text
-};
-
 FrequencyCounter.prototype.splitWords = function() {
   return this.text.split(" ")
 }
-
-var fc = new FrequencyCounter(bookText)
-console.log(fc.splitWords())
 
